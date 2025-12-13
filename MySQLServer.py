b63125ac963a304da0b3944db6b4c0067ae3d0cb
Gogo <gogoshedrack@gmail.com>
2025-12-13 17:31:13 +0100

    Fix exception handling for MySQL checker

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,22 +1,20 @@
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="Edekodov1@"
+            password="your_password"
         )
 
-        if connection.is_connected():
-            cursor = connection.cursor()
-            cursor.execute(
-                "CREATE DATABASE IF NOT EXISTS alx_book_store"
-            )
-            print("Database 'alx_book_store' created successfully!")
+        cursor = connection.cursor()
+        cursor.execute(
+            "CREATE DATABASE IF NOT EXISTS alx_book_store"
+        )
+        print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
